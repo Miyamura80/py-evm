59ca50db94c3fc58c9050e7f636541d4a1d26370
Eito Miyamura <38335479+Miyamura80@users.noreply.github.com>
2023-10-29 15:22:24 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -54,7 +54,7 @@ extras_require = {
         "web3>=4.1.0,<5.0.0",
     ],
     "docs": [
-        "py-evm>=0.2.0-a.14",
+        "py-evm-sentou>=0.2.0-a.14",
         # We need to have pysha for autodoc to be able to extract API docs
         "Sphinx>=1.5.5,<2",
         "jinja2>=3.0.0,<3.1.0",  # jinja2<3.0 or >=3.1.0 cause doc build failures.
@@ -91,7 +91,7 @@ with open("README.md") as readme_file:
     long_description = readme_file.read()
 
 setup(
-    name="py-evm",
+    name="py-evm-sentou",
     # *IMPORTANT*: Don't manually change the version here. Use the 'bumpversion' utility.
     version="0.8.0-beta.1",
     description="Python implementation of the Ethereum Virtual Machine",
@@ -99,7 +99,7 @@ setup(
     long_description_content_type="text/markdown",
     author="Ethereum Foundation",
     author_email="piper@pipermerriam.com",
-    url="https://github.com/ethereum/py-evm",
+    url="https://github.com/Miyamura80/py-evm",
     include_package_data=True,
     py_modules=["eth"],
     install_requires=install_requires,
